53c90b94e6cdaa5b129f9f5854904ef260672f1a
Yago Dias <yago.dias1409@gmail.com>
2021-12-05 13:23:41 -0300

    Adiconando alterações iniciais

---
+++ src/components/Posts/index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts }) => {
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          title={post.title}
+          body={post.body}
+          id={post.id}
+          cover={post.cover}
+        />
+      ))}
+    </div>
+  );
+};
